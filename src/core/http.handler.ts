1f2aa878827b17ec6a73324f35722594c3ca6b93
Sourav <codexsourav404@gmail.com>
2025-05-06 20:24:17 +0530

    httpRequest Add Middleware Support

--- src/core/http.handler.ts
+++ src/core/http.handler.ts
@@ -1,6 +1,5 @@
-import express from "express";
+import { NextFunction, Request, Response } from "express";
 
-// Define allowed keys
 type AllowedKeys =
   | "params"
   | "query"
@@ -10,29 +9,43 @@ type AllowedKeys =
   | "signedCookies"
   | "response";
 
-// Ensure T only has the allowed keys
-export type REQ<T extends Partial<Record<AllowedKeys, any>>> = T;
+export type With<T extends Partial<Record<AllowedKeys, any>>> = Request<
+  T["params"],
+  T["response"],
+  T["body"],
+  T["query"]
+> & {
+  headers: T["headers"];
+  cookies: T["cookies"];
+  signedCookies: T["signedCookies"];
+};
 
+interface HttpRequestOptions {
+  middlewares?: (
+    | ((req: With<any>, res: Response<any>, next: NextFunction) => void)
+  )[];
+}
+
+// Refactored httpRequest function to support middleware chaining
 export const httpRequest = <T extends Partial<Record<AllowedKeys, any>>>(
   handler: (
-    req: express.Request<T["params"], T["response"], T["body"], T["query"]> & {
-      headers: T["headers"];
-      cookies: T["cookies"];
-      signedCookies: T["signedCookies"];
-    },
-    res: express.Response<T["response"]>,
-    next: express.NextFunction
-  ) => Promise<any> | any
+    req: With<T>,
+    res: Response<T["response"]>,
+    next: NextFunction
+  ) => Promise<any> | any,
+  options?: HttpRequestOptions
 ) => {
-  return (
-    req: express.Request<T["params"], T["response"], T["body"], T["query"]> & {
-      headers: T["headers"];
-      cookies: T["cookies"];
-      signedCookies: T["signedCookies"];
-    },
-    res: express.Response<T["response"]>,
-    next: express.NextFunction
+  const middlewares = options?.middlewares || [];
+
+  const wrappedHandler = (
+    req: With<T>,
+    res: Response<T["response"]>,
+    next: NextFunction
   ) => {
+    for (const middleware of middlewares) {
+      middleware(req, res, next);
+    }
+
     try {
       const result = handler(req, res, next);
       if (result instanceof Promise) {
@@ -42,4 +55,14 @@ export const httpRequest = <T extends Partial<Record<AllowedKeys, any>>>(
       next(error);
     }
   };
+
+  // Adding .middleware() method to chain middlewares directly
+  wrappedHandler.middleware = (
+    middleware: (req: With<T>, res: Response<T["response"]>, next: NextFunction) => void
+  ) => {
+    middlewares.push(middleware);
+    return wrappedHandler;
+  };
+
+  return wrappedHandler;
 };
